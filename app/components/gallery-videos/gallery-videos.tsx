b5ddd4f23f4de88a85e034e308f175240cf47ade
Raidenshi <kiker200n@gmail.com>
2023-03-30 03:05:25 +0300

    youtube embed little fix :)

--- app/components/gallery-videos/gallery-videos.tsx
+++ app/components/gallery-videos/gallery-videos.tsx
@@ -12,7 +12,7 @@ export default function GalleryVideos({ videos }: { videos: Array<string> }) {
             <iframe
               width="560"
               height="315"
-              src={`https://www.youtube.com/embed/${id}`}
+              src={`https://www.youtube-nocookie.com/embed/${id}`}
               title="YouTube video player"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
