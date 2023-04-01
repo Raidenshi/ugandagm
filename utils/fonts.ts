617ba99fceaa1613e20b35727ba5751fe106b338
Raidenshi <kiker200n@gmail.com>
2023-04-01 15:18:02 +0300

    font fix

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,5 +1,8 @@
 import localFont from '@next/font/local';
 import { Work_Sans } from '@next/font/google';
 
-export const tribeca = localFont({ src: '../fonts/TRIBECA_.woff' });
+export const tribeca = localFont({
+  src: '../fonts/TRIBECA_.woff',
+  display: 'swap',
+});
 export const workSans = Work_Sans({ subsets: ['latin'] });
